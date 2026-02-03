2f6f09cf34f6f71038374e21197c5fd6928cf780
mironintelligence <mironintelligence@gmail.com>
2026-02-04 00:04:15 +0300

    fix: standardization of next-intl middleware and root layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
-import { locales } from "./navigation";
+import createMiddleware from 'next-intl/middleware';
+import { locales, localePrefix } from './navigation';
 
-export default function middleware(request: NextRequest) {
-    // Check if there is any supported locale in the pathname
-    const pathname = request.nextUrl.pathname;
-    const pathnameIsMissingLocale = locales.every(
-        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    );
-
-    // Redirect if there is no locale
-    if (pathnameIsMissingLocale) {
-        const locale = "tr";
-        return NextResponse.redirect(
-            new URL(`/${locale}${pathname}`, request.url)
-        );
-    }
-}
+export default createMiddleware({
+    // A list of all locales that are supported
+    locales,
+    localePrefix,
+    // Used when no locale matches
+    defaultLocale: 'tr'
+});
 
 export const config = {
-    matcher: [
-        // Skip all internal paths (_next)
-        '/((?!api|_next/static|_next/image|images|favicon.ico).*)',
-    ],
+    // Match only internationalized pathnames
+    matcher: ['/', '/(tr|en)/:path*', '/((?!api|_next|_vercel|images|.*\\..*).*)']
 };
